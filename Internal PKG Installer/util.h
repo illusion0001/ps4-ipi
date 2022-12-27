018680575d606d3486cedb1a6ef20a0442a6b9b5
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-12-27 10:23:16 -0600

    Update util.h

--- Internal PKG Installer/util.h
+++ Internal PKG Installer/util.h
@@ -4,6 +4,5 @@
 #include <orbis/libkernel.h>
 #include <fcntl.h>
 
-
-int system_notification(const char* text, const char* iconName = "icon_system");
+int system_notification(const char* text, const char* iconName);
 int ends_with(const char* haystack, const char* needle);
