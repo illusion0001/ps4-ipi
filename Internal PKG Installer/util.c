16f0e538c4004980e7663535a0a275eb171cd4ab
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-12-27 10:22:52 -0600

    Update util.c

--- Internal PKG Installer/util.c
+++ Internal PKG Installer/util.c
@@ -2,36 +2,29 @@
 
 int system_notification(const char* text, const char* iconName) {
 	OrbisNotificationRequest NotificationBuffer;
-	
-	NotificationBuffer.type = OrbisNotificationRequestType::NotificationRequest;
+	NotificationBuffer.type = 0;
 	NotificationBuffer.unk3 = 0; 
 	NotificationBuffer.useIconImageUri = 1;
 	NotificationBuffer.targetId = -1;
-	
-	snprintf(NotificationBuffer.iconUri, sizeof(NotificationBuffer.iconUri), "cxml://psnotification/tex_%s", "icon_system");
+	snprintf(NotificationBuffer.iconUri, sizeof(NotificationBuffer.iconUri), "cxml://psnotification/tex_icon_system");
 	strncpy(NotificationBuffer.message, text, sizeof(NotificationBuffer.message));
-	sceKernelSendNotificationRequest(0, &NotificationBuffer, 3120, 0);
-
+	sceKernelSendNotificationRequest(0, &NotificationBuffer, sizeof(NotificationBuffer), 0);
 	return 0;
 }
 
 int ends_with(const char* haystack, const char* needle) {
 	ptrdiff_t diff;
 	int i;
-
 	assert(haystack != NULL);
 	assert(needle != NULL);
-
 	diff = strlen(haystack) - strlen(needle);
 	if (diff < 0) {
 		return -1;
 	}
-
 	for (i = 0; needle[i] != '\0'; ++i) {
 		if (needle[i] != haystack[i + diff]) {
 			return -2;
 		}
 	}
-
 	return 0;
 }
